93b2b56cef5783f8928d0285cc7428510d77ae98
dtzr09 <derricktay17@gmail.com>
2025-06-14 00:49:07 +0800

    adding prod and dev mode

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from dotenv import load_dotenv
 import os
 import logging
 from handlers.setup import setup_handlers, set_bot_commands
-
+import asyncio
+# 
 # Load environment variables
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+MODE = os.getenv("MODE", "dev")
+PORT = int(os.environ.get("PORT", 8443)) 
+RAILWAY_DOMAIN = os.getenv("RAILWAY_DOMAIN")  
 
 # Logging
 logging.basicConfig(
@@ -18,9 +22,20 @@ def main():
     app = ApplicationBuilder().token(BOT_TOKEN).post_init(set_bot_commands).build()
     setup_handlers(app)
 
-    logging.info("Bot started...")
+    logging.info(f"Bot running in {MODE} mode...")
 
-    app.run_polling()
+    if MODE == "prod":
+        app.run_webhook(
+            listen="0.0.0.0",
+            port=PORT,
+            url_path="/webhook",
+            webhook_url=f"https://{RAILWAY_DOMAIN}/webhook"
+        )
+    else:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(app.bot.delete_webhook(drop_pending_updates=True))
+        app.run_polling()
 
 if __name__ == "__main__":
     main()
