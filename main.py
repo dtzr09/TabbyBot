1acc4d71a30ff9217858a9d2484a97e6de299056
dtzr09 <derricktay17@gmail.com>
2025-06-14 00:16:10 +0800

    remove mode

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 import os
 import logging
 from handlers.setup import setup_handlers, set_bot_commands
-import asyncio
 
 # Load environment variables
 load_dotenv()
@@ -15,23 +14,14 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-MODE = os.getenv("MODE", "dev")  # "prod" or "dev"
-
-
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     setup_handlers(app)
 
     logging.info("Bot started...")
 
-    if MODE == "prod":
-        # Let Railway run the webhook or polling
-        app.run_polling()
-    else:
-        # Local dev mode: clear webhook first
-        asyncio.run(app.bot.delete_webhook(drop_pending_updates=True))
-        app.run_polling()
-
+    app.run_polling()
+       
     set_bot_commands(app)
 
 if __name__ == "__main__":
